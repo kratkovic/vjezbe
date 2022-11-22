10c6c729c53b97389f1b54ed13b147d8e2673b5e
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2022-11-22 19:25:06 +0100

    nadopuna vjezbe

--- SQL/vjezba01.sql
+++ SQL/vjezba01.sql
@@ -8,18 +8,13 @@ use samouprava;
 create table zupanija(
     sifra int not null primary key auto_increment,
     naziv varchar(50),
-    zupan int
+    zupan int,
+    opcina int
 );
-
-create table zupan(
-    sifra int not null primary key auto_increment,
-    ime varchar(50),
-    prezime varchar(50)
-);
-
 create table opcina(
     sifra int not null primary key auto_increment,
     zupanija int,
+    mjesto int,
     naziv varchar(50)
 );
 
@@ -29,6 +24,19 @@ create table mjesto(
     naziv varchar(50)
 );
 
-alter table zupanija add foreign key (zupan) references zupan(sifra);
-alter table opcina add foreign key (zupanija) references zupanija(sifra);
-alter table mjesto add foreign key (opcina) references opcina(sifra);
+create table zupan(
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50),
+    zupanija int
+);
+
+alter table zupan add foreign key (zupanija) references zupanija(sifra);
+alter table opcina add foreign key (mjesto) references mjesto(sifra);
+alter table zupanija add foreign key (opcina) references opcina(sifra);
+
+
+
+
+
+ 
