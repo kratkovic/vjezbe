8b720f4343e637adb2fb09edb56ae8b5b06fb58b
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2022-12-05 20:35:21 +0100

    dodavanje not null

--- SQL/kolokvij_vjezba_1.sql
+++ SQL/kolokvij_vjezba_1.sql
@@ -10,52 +10,52 @@ use obitelj;
 create table sestra(
     sifra int not null primary key auto_increment,
     introvertno bit,
-    haljina varchar(31),
+    haljina varchar(31) not null,
     maraka decimal(16,6),
-    hlace varchar(46),
-    narukvica int
+    hlace varchar(46) not null,
+    narukvica int not null
 );
 
 create table zena(
     sifra int not null primary key auto_increment,
     treciputa datetime,
     hlace varchar(46),
-    kratkamajica varchar(31),
-    jmbag char(11),
-    bojaociju varchar(39),
+    kratkamajica varchar(31) not null,
+    jmbag char(11) not null,
+    bojaociju varchar(39) not null,
     haljina varchar(44),
-    sestra int
+    sestra int not null
 );
 
 create table muskarac(
     sifra int not null primary key auto_increment,
-    bojaociju varchar(50),
+    bojaociju varchar(50) not null,
     hlace varchar(30),
     modelnaocala varchar(43),
-    maraka decimal(14,5),
-    zena int
+    maraka decimal(14,5) not null,
+    zena int not null
 );
 
 create table mladic(
     sifra int not null primary key auto_increment,
-    suknja varchar(50),
-    kuna decimal(16.8),
+    suknja varchar(50) not null,
+    kuna decimal(16.8) not null,
     drugiputa datetime,
     asocijalno bit,
-    ekstroventno bit,
-    duskerica varchar(48),
+    ekstroventno bit not null,
+    duskerica varchar(48) not null,
     muskarac int
 );
 
 create table sestra_svekar(
     sifra int not null primary key auto_increment,
-    sestra int,
-    svekar int
+    sestra int not null,
+    svekar int not null
 );
 
 create table svekar(
     sifra int not null primary key auto_increment,
-    bojaociju varchar(40),
+    bojaociju varchar(40) not null,
     prstena int,
     dukserica varchar(41),
     lipa decimal(13,8),
@@ -67,15 +67,15 @@ create table punac(
     sifra int not null primary key auto_increment,
     ogrlica int,
     gustoca decimal(14,9),
-    hlace varchar(41)
+    hlace varchar(41) not null
 );
 
 create table cura(
     sifra int not null primary key auto_increment,
-    novcica decimal(16,5),
-    gustoca decimal(18,6),
+    novcica decimal(16,5) not null,
+    gustoca decimal(18,6) not null,
     lipa decimal(13,10),
-    ogrlica int,
+    ogrlica int not null,
     bojakosa varchar(38),
     suknja varchar(36),
     punac int
@@ -92,7 +92,7 @@ alter table sestra_svekar add foreign key (svekar) references svekar(sifra);
 
 
 insert into muskarac(hlace,bojaociju,modelnaocala)
-values('traperice','zelena',1), 
+values('traperice','zelena',4), 
 ('svecane','plava',2),
 ('chino','smedja',3);
 
@@ -128,3 +128,7 @@ inner join svekar f on e.sestra = f.sifra
 where c.hlace like ('%a') and d.haljina like ('%ba')
 order by 3 desc;
 
+select a.haljina, a.maraka
+from sestra a
+left join sestra_svekar b on a.sifra = b.sestra
+where b.sestra is null;
