da6e941370082092e1b4a4fca9bed176f3fd753e
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2022-12-27 17:28:07 +0100

    zavrsena vjezba

--- SQL/vjezba06.sql
+++ SQL/vjezba06.sql
@@ -40,3 +40,27 @@ create table sponzor(
 alter table kustos add foreign key (izlozba) references izlozba(sifra);
 alter table djelo add foreign key (izlozba) references izlozba(sifra);
 alter table izlozba add foreign key (sponzor) references sponzor(sifra);
+
+
+insert into djelo(sifra,autor,vrijednost,tehnika)
+values(null,'Rothko',5000.00,'Akvarel'),
+(null,'Bacon',3000.00,'Tempera'),
+(null,'Ruscha',11100.00,'Enkaustika');
+
+insert into kustos(sifra,ime,prezime,oib,iban)
+values(null,'Marin','Pavic','54746925784', 'HR8924840081102131080'),
+(null,'Miljenko','Drzic', '95277199584','HR9924980064102531028');
+
+
+insert into sponzor(sifra,naziv)
+values(null,'Arena'),
+(null,'Pulaplus'),
+(null,'24sata');
+
+
+insert into izlozba (sifra,naziv,iznos,sponzor,kustos)
+values(null,'Pogon jedistva', 500000.00,1,2),
+(null,'Izazov moderne', 900000.00,1,1);
+
+insert into izlozba_djelo (djelo,izlozba)
+values(1,2),(1,1);
