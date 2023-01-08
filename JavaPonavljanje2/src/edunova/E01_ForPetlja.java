5269eb1e67a379a6a670da17ad33ce4f2b13ea40
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-01-08 21:21:49 +0100

    Update E01_ForPetlja.java

--- JavaPonavljanje2/src/edunova/E01_ForPetlja.java
+++ JavaPonavljanje2/src/edunova/E01_ForPetlja.java
@@ -21,51 +21,51 @@ public class E01_ForPetlja {
 		for (int i = 0; i < 10; i = i + 1) {
 			System.out.println("Osijek");
 		}
-        for (int i=0; i<10; i=i+1) {
-        	System.out.println(i);
-        }
-        
-        // zbroj prvih 100 brojeva
-        int zbroj = 0;
-        for (int i =1; i<=100; i=i+1) {
-        	zbroj = zbroj + i;
-        	if (i!=100) {
-        		System.out.println(zbroj + "+" + (i+1));
-        	}else {
-        		System.out.println(zbroj);
-        	}
-        	
-        }
-       
-    	int niz[] = new int[2];
+		for (int i = 0; i < 10; i = i + 1) {
+			System.out.println(i);
+		}
+
+		// zbroj prvih 100 brojeva
+		int zbroj = 0;
+		for (int i = 1; i <= 100; i = i + 1) {
+			zbroj = zbroj + i;
+			if (i != 100) {
+				System.out.println(zbroj + "+" + (i + 1));
+			} else {
+				System.out.println(zbroj);
+			}
+
+		}
+
+		int niz[] = new int[2];
 		// unos vrijednosti za sve elemente niza
 		for (int g = 0; g < niz.length; g++) {
 
 			niz[g] = Integer.parseInt(JOptionPane.showInputDialog("Unesi " + (g + 1) + ". broj"));
 		}
-        
-      //zbrojite vrijednosti svih elementa niza i ispiste zbroj
-        
-        zbroj = 0;
-        for (int g=0; g<niz.length; g++) {
-        	zbroj += niz[g];
-        	System.out.println(zbroj);
-        }
-   
-       String grad = "Osijek";
-       
-       for(int i =0; i<grad.length();i++) {
-    	   System.out.println(grad.charAt(i));
-       }
-       
-       //ugnježđivanje petlji
-       
-       for (int i = 1; i<10; i++) {
-    	   for(int j=1; j<10;j++) {
-    		   System.out.print(i * j + "  ");
-    	   }
-    	   System.out.println();
-    	   
-       }
+
+		// zbrojite vrijednosti svih elementa niza i ispiste zbroj
+
+		zbroj = 0;
+		for (int g = 0; g < niz.length; g++) {
+			zbroj += niz[g];
+			System.out.println(zbroj);
+		}
+
+		String grad = "Osijek";
+
+		for (int i = 0; i < grad.length(); i++) {
+			System.out.println(grad.charAt(i));
+		}
+
+		// ugnježđivanje petlji
+
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.print(i * j + "  ");
+			}
+			System.out.println();
+
+		}
 	}
 }
