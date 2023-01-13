d808a41d8ec37586dbbbf312d986bdac445605ea
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-01-13 21:27:50 +0100

    drugi izgled koda

--- Java/Edunova5/src/edunova/Z02_TablicaMnozena.java
+++ Java/Edunova5/src/edunova/Z02_TablicaMnozena.java
@@ -4,23 +4,25 @@ public class Z02_TablicaMnozena {
 	public static void main(String[] args) {
 		// Kreirati program koji ispisuje tablicu množenja 15x15
 
-		for (int i = 1; i <= 15; i++) {
-			for (int j = 1; j <= 15; j++) {
-				if ((i * j < 10)) {
-					System.out.print("    " + i * j);
-				} else if ((i * j > 100)) {
-					System.out.print("  " + i * j);
-				} else if ((i * j < 100)) {
-					System.out.print("   " + i * j);
-				} else if ((i * j > 10) && (i * j < 100)) {
-					System.out.print("  " + i * j);
-				} else {
-					System.out.print("  "  + i*j);
-				}
-
+		int tablicamnozenja = 15;
+		ispisitablicu(tablicamnozenja);
+	}
+		public static void ispisitablicu(int tablicamnozenja) {
+			System.out.format("      ");
+			for (int i = 1; i<=tablicamnozenja; i++) {
+				System.out.format("%4d", i);
+		}
+		System.out.println();
+		System.out.println("------------------------------------------------------------------");
+		
+		for (int i = 1; i<=tablicamnozenja; i++) {
+			System.out.format("%4d |", i);
+			for (int j=1; j<=tablicamnozenja; j++) {
+				System.out.format("%4d", i*j);
 			}
 			System.out.println();
 		}
+		
 
 	}
 }
