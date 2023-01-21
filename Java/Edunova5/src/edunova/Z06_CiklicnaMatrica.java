485676e8cb4a398bc123f26f3327500e9b0a7c57
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-01-21 20:58:05 +0100

    zavrsena matrica

--- Java/Edunova5/src/edunova/Z06_CiklicnaMatrica.java
+++ Java/Edunova5/src/edunova/Z06_CiklicnaMatrica.java
@@ -8,35 +8,56 @@ public class Z06_CiklicnaMatrica {
 		int r = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova"));
 		int s = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
 		int mat[][] = new int[r][s];
-		int PocRed = 0;
-		int KrajRed = r;
-		int PocStupac = 0;
-		int KrajStupac = s;
-		while (PocRed < KrajRed && PocStupac < KrajStupac) {
-			for (int i = PocStupac; i < KrajStupac; ++i) {
-				mat[PocRed][i] = broj++;
-			}
-			PocRed++;
-			for (int i = PocRed; i < KrajRed; ++i) {
-				mat[i][KrajStupac - 1] = broj++;
-			}
-			KrajStupac--;
-			if (PocRed < KrajRed) {
-				for (int i = KrajStupac - 1; i >= PocStupac; --i) {
-					mat[KrajRed - 1][i] = broj++;
-				}
-				KrajRed--;
-				if (PocStupac < KrajStupac) {
-					for (int i = KrajRed - 1; i >= PocRed; --i) {
-						mat[i][PocStupac] = broj++;
-					}
-					PocStupac++;
+
+		int PocRed = r - 1;
+		int KrajRed = 0;
+		int PocStupac = s - 1;
+		int KrajStupac = 0;
+
+		while (broj <= r * s) {
+			for (int z = PocStupac; z >= KrajStupac; z--) {
+				if (broj > r * s) {
+					break;
+				} else {
+					mat[PocRed][z] = broj++;
 				}
 			}
+			PocRed--;
+			
+			for(int z = PocRed; z>=KrajRed; z--){
+				if (broj > r *s) {
+					break;
+			}else {
+				mat[z][KrajStupac] = broj ++;
+
+	
+				}
+			}
+			KrajStupac++;
+			
+			for(int z = KrajStupac; z<=PocStupac;z++) {
+				if (broj > r * s) {
+					break;
+				}else {
+					mat[KrajRed][z] = broj ++;
+					
+				}
+			}
+			
+			KrajRed++;
+			for(int z = KrajRed; z<=PocRed; z++) {
+				if (broj> r*s) {
+					break;
+				}else {
+					mat[z][PocStupac] = broj++;
+				}
+			}
+			PocStupac--;
 		}
+		
 		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < s; j++) {
-				System.out.print(mat[i][j] + " ");
+			for (int z = 0; z < s; z++) {
+				System.out.print(mat[i][z] + "   ");
 			}
 			System.out.println();
 		}
