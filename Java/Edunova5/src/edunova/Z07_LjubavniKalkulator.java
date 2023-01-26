a9061685293be6f873a68cc21efa3efb77ce7ab5
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-01-26 19:39:16 +0100

    unosenje imena

---
+++ Java/Edunova5/src/edunova/Z07_LjubavniKalkulator.java
@@ -0,0 +1,17 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z07_LjubavniKalkulator {
+	
+	public static void main(String[] args) {
+		
+		String ime1 = JOptionPane.showInputDialog("Unesi svoje ime");
+		String ime2 = JOptionPane.showInputDialog("Unesi ime simpatije");
+		
+		String broj = "";
+		
+		System.out.println(ime1 + " i " + ime2 + " se vole " + broj + "%");
+	}
+
+}
