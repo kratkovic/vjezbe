00fd00088c275e354c96009bf665ea184af1f5ab
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-03-02 16:29:55 +0100

    uredivanje

--- Java/netbeans/EdunovaAPP/src/main/java/edunova/controller/ObradaSmjer.java
+++ Java/netbeans/EdunovaAPP/src/main/java/edunova/controller/ObradaSmjer.java
@@ -9,10 +9,7 @@ import edunova.util.EdunovaException;
 import java.math.BigDecimal;
 import java.util.List;
 
-/**
- *
- * @author dell
- */
+
 public class ObradaSmjer extends Obrada<Smjer>{
 
     @Override
