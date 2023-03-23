c8624bb8a33fb6c99b0be2f72a9267bc4ed89d54
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-03-23 08:22:05 +0100

    import uredjen

--- Java/netbeans/EdunovaAPP/src/main/java/edunova/view/ProzorIzbornik.java
+++ Java/netbeans/EdunovaAPP/src/main/java/edunova/view/ProzorIzbornik.java
@@ -15,8 +15,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import org.hibernate.Session;
 import org.jfree.chart.ChartFactory;
